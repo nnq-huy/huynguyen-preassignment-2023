dad663e2996896d846f70931e3bb012de555087a
huyggsii <quanghuy.nguyennhu@gmail.com>
2023-01-19 20:38:27 +0200

    added List Menu to import data(view only)
    functions will be added later

--- react-native-front-end/src/screens/HomeScreen.tsx
+++ react-native-front-end/src/screens/HomeScreen.tsx
@@ -1,10 +1,45 @@
 //Home screen, contains links to app's main functions: data import, data entry
-import { Text } from "react-native-paper";
 import { View, StyleSheet } from "react-native";
+import { Appbar, Divider, List } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
   return (
-    <View style={styles.container}>
-      <Text variant="displayMedium">Home</Text>
+    <View>
+      <Appbar mode="center-aligned" safeAreaInsets={{ top }}>
+        <Appbar.Content title="Home" />
+      </Appbar>
+      <View style={styles.container}>
+        <List.Section>
+          <List.Subheader>Add data from csv</List.Subheader>
+          <List.Item
+            title="Upload new stations list(csv)"
+            left={() => <List.Icon icon="file-delimited" />}
+            onPress={() => {}}
+          />
+          <Divider bold={true} />
+          <List.Item
+            title="Upload new journey list(csv)"
+            left={() => <List.Icon icon="file-delimited" />}
+            onPress={() => {}}
+          />
+        </List.Section>
+        <List.Section>
+          <List.Subheader>Add data manually</List.Subheader>
+          <List.Item
+            title="Enter a new station"
+            left={() => <List.Icon icon="map-marker" />}
+            onPress={() => {}}
+          />
+          <Divider bold={true} />
+          <List.Item
+            title="Enter a new journey "
+            left={() => <List.Icon icon="bike" />}
+            onPress={() => {}}
+          />
+        </List.Section>
+      </View>
     </View>
   );
 };
@@ -12,9 +47,8 @@ const HomeScreen = () => {
 export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "#E5E0FF",
+    margin: 10,
   },
 });
