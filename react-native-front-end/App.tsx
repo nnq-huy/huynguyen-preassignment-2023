38598668435ba56a115716eab51509e7dbc09d36
huyggsii <quanghuy.nguyennhu@gmail.com>
2023-01-19 18:00:28 +0200

    added bottom navigation

--- react-native-front-end/App.tsx
+++ react-native-front-end/App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { BottomNavigation } from "react-native-paper";
+
+import { HomeScreen, JourneyScreen, StationScreen } from "./src/screens";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {
+      key: "home",
+      title: "Home",
+      focusedIcon: "home",
+      unfocusedIcon: "home-outline",
+    },
+    { key: "station", title: "Stations", focusedIcon: "sign-real-estate" },
+    { key: "journey", title: "Journeys", focusedIcon: "bicycle" },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: HomeScreen,
+    station: StationScreen,
+    journey: JourneyScreen,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider>
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
