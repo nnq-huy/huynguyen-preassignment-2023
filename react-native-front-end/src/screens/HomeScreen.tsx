77151c36d7470f2c915acec45092bafe426de8cd
huyggsii <quanghuy.nguyennhu@gmail.com>
2023-01-23 22:54:35 +0200

    Update HomeScreen.tsx
    rename variables

--- react-native-front-end/src/screens/HomeScreen.tsx
+++ react-native-front-end/src/screens/HomeScreen.tsx
@@ -1,11 +1,22 @@
 //Home screen, contains links to app's main functions: data import, data entry
 import React, { useState } from "react";
-import { View, StyleSheet, Alert, Button } from "react-native";
-import { Appbar, Divider, List } from "react-native-paper";
+import { View, StyleSheet, Alert } from "react-native";
+import {
+  Appbar,
+  Button,
+  Dialog,
+  Divider,
+  List,
+  Provider,
+  Portal,
+  Text,
+  TextInput,
+} from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import serverUrl from "../utils/backend";
 import axios from "axios";
 import * as DocumentPicker from "expo-document-picker";
+import { Station } from "../utils/types";
 
 const HomeScreen = () => {
   const [uploadStationsTitle, setUploadStationsTitle] = useState(
@@ -16,7 +27,22 @@ const HomeScreen = () => {
   );
   const [pushStationsStatus, setPushStationsStatus] = useState(true);
   const [pushJourneysStatus, setPushJourneyStationsStatus] = useState(true);
+  const [visible, setVisible] = useState(false);
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+  const [stationId, setStationId] = useState("");
+  const [stationName, setStationName] = useState("");
+  const [stationAddress, setStationAddress] = useState("");
+  const [stationLatitude, setStationLatitude] = useState("");
+  const [stationLongitude, setStationLongitude] = useState("");
 
+  const  resetStationInput= ()=> {
+    setStationId("");
+    setStationName("");
+    setStationAddress("");
+    setStationLatitude("");
+    setStationLongitude("");
+  }
   const handleUploadCsv = (csvType: String) => {
     let formData = new FormData();
     let url = serverUrl + "/upload/" + csvType;
@@ -44,7 +70,6 @@ const HomeScreen = () => {
           });
           if (responseOfFileUpload.status === 200) {
             let fileName = responseOfFileUpload.data.fileName;
-
             if (csvType === "stations") {
               setUploadStationsTitle(fileName + " uploaded");
               setPushStationsStatus(false);
@@ -75,67 +100,129 @@ const HomeScreen = () => {
       Alert.alert(url, "error: " + err);
     }
   };
+  const putNewStation = async () => {
+    let newStation: Station = {
+      id: parseInt(stationId),
+      name: stationName,
+      address: stationAddress,
+      x: parseFloat(stationLongitude),
+      y: parseFloat(stationLatitude),
+    };
+    const url = serverUrl + "/stations/new";
+    try {
+      const responseOfDbQuery = await axios.post(url, newStation);
+      if (responseOfDbQuery.status === 201) {
+        hideDialog();
+        Alert.alert(url, responseOfDbQuery.data);
+        resetStationInput();
+      }
+    } catch (err) {
+      hideDialog();
+      Alert.alert(url, "error: " + err);
+    }
+  };
   const { top } = useSafeAreaInsets();
   return (
-    <View>
-      <Appbar mode="center-aligned" safeAreaInsets={{ top }}>
-        <Appbar.Content title="Home" />
-      </Appbar>
-      <View style={styles.container}>
-        <List.Section>
-          <List.Subheader>Add data from csv</List.Subheader>
-          <List.Item
-            title={uploadStationsTitle}
-            left={() => <List.Icon icon="file-delimited" />}
-            onPress={() => {
-              handleUploadCsv("stations");
-            }}
-            right={() => (
-              <Button
-                color="#8EA7E9"
-                title="Push to DB"
-                disabled={pushStationsStatus}
-                onPress={() => {
-                  pushCsvToDb("stations");
-                }}
-              />
-            )}
-          />
-          <Divider bold={true} />
-          <List.Item
-            title={uploadJourneysTitle}
-            left={() => <List.Icon icon="file-delimited" />}
-            onPress={() => {
-              handleUploadCsv("journeys");
-            }}
-            right={() => (
-              <Button
-                color="#8EA7E9"
-                title="Push to DB"
-                disabled={pushJourneysStatus}
-                onPress={() => {
-                  pushCsvToDb("journeys");
-                }}
-              />
-            )}
-          />
-        </List.Section>
-        <List.Section>
-          <List.Subheader>Add data manually</List.Subheader>
-          <List.Item
-            title={"Enter a new station"}
-            left={() => <List.Icon icon="map-marker" />}
-            onPress={() => {}}
-          />
-          <Divider bold={true} />
-          <List.Item
-            title="Enter a new journey "
-            left={() => <List.Icon icon="bike" />}
-            onPress={() => {}}
-          />
-        </List.Section>
+    <Provider>
+      <View>
+        <Appbar mode="center-aligned" safeAreaInsets={{ top }}>
+          <Appbar.Content title="Home" />
+        </Appbar>
+        <View style={styles.container}>
+          <List.Section>
+            <List.Subheader>Add data from csv</List.Subheader>
+            <List.Item
+              title={uploadStationsTitle}
+              left={() => <List.Icon icon="file-delimited" />}
+              onPress={() => {
+                handleUploadCsv("stations");
+              }}
+              right={() => (
+                <Button
+                  color="#8EA7E9"
+                  disabled={pushStationsStatus}
+                  onPress={() => {
+                    pushCsvToDb("stations");
+                  }}
+                >
+                  Push to DB
+                </Button>
+              )}
+            />
+            <Divider bold={true} />
+            <List.Item
+              title={uploadJourneysTitle}
+              left={() => <List.Icon icon="file-delimited" />}
+              onPress={() => {
+                handleUploadCsv("journeys");
+              }}
+              right={() => (
+                <Button
+                  color="#8EA7E9"
+                  disabled={pushJourneysStatus}
+                  onPress={() => {
+                    pushCsvToDb("journeys");
+                  }}
+                >
+                  Push to DB
+                </Button>
+              )}
+            />
+          </List.Section>
+          <List.Section>
+            <List.Subheader>Add data manually</List.Subheader>
+            <List.Item
+              title={"Enter a new station"}
+              left={() => <List.Icon icon="map-marker" />}
+              onPress={showDialog}
+            />
+            <Divider bold={true} />
+            <List.Item
+              title="Enter a new journey "
+              left={() => <List.Icon icon="bike" />}
+              onPress={() => {}}
+            />
+          </List.Section>
+          <Portal>
+            <Dialog visible={visible} onDismiss={hideDialog}>
+              <Dialog.Title>New Station</Dialog.Title>
+              <Dialog.Content>
+                <Text variant="bodyMedium">Enter new station details:</Text>
+                <TextInput
+                  label="Station id"
+                  value={stationId}
+                  onChangeText={(text) => setStationId(text)}
+                />
+                <TextInput
+                  label="Station name"
+                  value={stationName}
+                  onChangeText={(text) => setStationName(text)}
+                />
+                <TextInput
+                  label="Station address"
+                  value={stationAddress}
+                  onChangeText={(text) => setStationAddress(text)}
+                />
+                <TextInput
+                  label="Latitude"
+                  value={stationLatitude}
+                  onChangeText={(text) => setStationLatitude(text)}
+                />
+                <TextInput
+                  label="Longitude"
+                  value={stationLongitude}
+                  onChangeText={(text) => setStationLongitude(text)}
+                />
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button color="#8EA7E9"  onPress={hideDialog} >Cancel</Button>
+                <Button color="#8EA7E9"  onPress={putNewStation} >Ok</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        </View>
       </View>
-    </View>
+    </Provider>
   );
 };
 
@@ -145,5 +232,9 @@ const styles = StyleSheet.create({
     alignContent: "center",
     backgroundColor: "#E5E0FF",
     margin: 10,
+  },
+  fixToText: {
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
 });
