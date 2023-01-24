ec640761da3693c75f0001e57fda5de57820f460
huyggsii <quanghuy.nguyennhu@gmail.com>
2023-01-24 21:21:41 +0200

    added new journey add interface

--- react-native-front-end/src/screens/HomeScreen.tsx
+++ react-native-front-end/src/screens/HomeScreen.tsx
@@ -16,7 +16,8 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import serverUrl from "../utils/backend";
 import axios from "axios";
 import * as DocumentPicker from "expo-document-picker";
-import { Station } from "../utils/types";
+import { Station, Journey } from "../utils/types";
+import { MaskedTextInput } from "react-native-mask-text";
 
 const HomeScreen = () => {
   const [uploadStationsTitle, setUploadStationsTitle] = useState(
@@ -27,22 +28,26 @@ const HomeScreen = () => {
   );
   const [pushStationsStatus, setPushStationsStatus] = useState(true);
   const [pushJourneysStatus, setPushJourneyStationsStatus] = useState(true);
-  const [visible, setVisible] = useState(false);
-  const showDialog = () => setVisible(true);
-  const hideDialog = () => setVisible(false);
-  const [stationId, setStationId] = useState("");
-  const [stationName, setStationName] = useState("");
-  const [stationAddress, setStationAddress] = useState("");
-  const [stationLatitude, setStationLatitude] = useState("");
-  const [stationLongitude, setStationLongitude] = useState("");
+  const [newStationDialogVisible, setStationVisible] = useState(false);
+  const [newStation, setNewStation] = useState({
+    id: "",
+    name: "",
+    address: "",
+    x: "",
+    y: "",
+  });
+  const [newJourneyDialogVisible, setJourneyVisible] = useState(false);
+  const [newJourney, setNewJourney] = useState({
+    departure_time: "",
+    return_time: "",
+    departure_station_id: "",
+    departure_station: "",
+    return_station_id: "",
+    return_station: "",
+    duration: "",
+    distance: "",
+  });
 
-  const  resetStationInput= ()=> {
-    setStationId("");
-    setStationName("");
-    setStationAddress("");
-    setStationLatitude("");
-    setStationLongitude("");
-  }
   const handleUploadCsv = (csvType: String) => {
     let formData = new FormData();
     let url = serverUrl + "/upload/" + csvType;
@@ -101,27 +106,53 @@ const HomeScreen = () => {
     }
   };
   const putNewStation = async () => {
-    let newStation: Station = {
-      id: parseInt(stationId),
-      name: stationName,
-      address: stationAddress,
-      x: parseFloat(stationLongitude),
-      y: parseFloat(stationLatitude),
+    let uploadStation: Station = {
+      id: parseInt(newStation.id),
+      name: newStation.name,
+      address: newStation.address,
+      x: parseFloat(newStation.x),
+      y: parseFloat(newStation.y),
     };
     const url = serverUrl + "/stations/new";
     try {
-      const responseOfDbQuery = await axios.post(url, newStation);
+      const responseOfDbQuery = await axios.post(url, uploadStation);
       if (responseOfDbQuery.status === 201) {
-        hideDialog();
+        setStationVisible(false);
         Alert.alert(url, responseOfDbQuery.data);
-        resetStationInput();
       }
     } catch (err) {
-      hideDialog();
+      setStationVisible(false);
+      Alert.alert(url, "error: " + err);
+    }
+  };
+  const putNewJourney = async () => {
+    let uploadJourney: Journey = {
+      departure_time: newJourney.departure_time,
+      return_time: newJourney.return_time,
+      departure_station_id: parseInt(newJourney.departure_station_id),
+      departure_station: newJourney.departure_station,
+      return_station_id: parseInt(newJourney.departure_station_id),
+      return_station: newJourney.return_station,
+      duration:
+        (Date.parse(newJourney.return_time) -
+          Date.parse(newJourney.departure_time)) /
+        1000,
+      distance: parseFloat(newJourney.distance),
+    };
+    const url = serverUrl + "/journeys/new";
+    try {
+      const responseOfDbQuery = await axios.post(url, uploadJourney);
+      if (responseOfDbQuery.status === 201) {
+        setJourneyVisible(false);
+        Alert.alert(url, responseOfDbQuery.data);
+      }
+    } catch (err) {
+      setJourneyVisible(false);
       Alert.alert(url, "error: " + err);
     }
   };
   const { top } = useSafeAreaInsets();
+
   return (
     <Provider>
       <View>
@@ -174,49 +205,154 @@ const HomeScreen = () => {
             <List.Item
               title={"Enter a new station"}
               left={() => <List.Icon icon="map-marker" />}
-              onPress={showDialog}
+              onPress={() => {
+                setStationVisible(true);
+              }}
             />
             <Divider bold={true} />
             <List.Item
               title="Enter a new journey "
               left={() => <List.Icon icon="bike" />}
-              onPress={() => {}}
+              onPress={() => {
+                setJourneyVisible(true);
+              }}
             />
           </List.Section>
           <Portal>
-            <Dialog visible={visible} onDismiss={hideDialog}>
+            <Dialog
+              visible={newStationDialogVisible}
+              onDismiss={() => {
+                setStationVisible(false);
+              }}
+            >
               <Dialog.Title>New Station</Dialog.Title>
               <Dialog.Content>
                 <Text variant="bodyMedium">Enter new station details:</Text>
                 <TextInput
                   label="Station id"
-                  value={stationId}
-                  onChangeText={(text) => setStationId(text)}
+                  onChangeText={(text) =>
+                    setNewStation({ ...newStation, id: text })
+                  }
                 />
                 <TextInput
                   label="Station name"
-                  value={stationName}
-                  onChangeText={(text) => setStationName(text)}
+                  onChangeText={(text) =>
+                    setNewStation({ ...newStation, name: text })
+                  }
                 />
                 <TextInput
                   label="Station address"
-                  value={stationAddress}
-                  onChangeText={(text) => setStationAddress(text)}
+                  onChangeText={(text) =>
+                    setNewStation({ ...newStation, address: text })
+                  }
                 />
                 <TextInput
                   label="Latitude"
-                  value={stationLatitude}
-                  onChangeText={(text) => setStationLatitude(text)}
+                  onChangeText={(text) =>
+                    setNewStation({ ...newStation, y: text })
+                  }
                 />
                 <TextInput
                   label="Longitude"
-                  value={stationLongitude}
-                  onChangeText={(text) => setStationLongitude(text)}
+                  onChangeText={(text) =>
+                    setNewStation({ ...newStation, x: text })
+                  }
                 />
               </Dialog.Content>
               <Dialog.Actions>
-                <Button color="#8EA7E9"  onPress={hideDialog} >Cancel</Button>
-                <Button color="#8EA7E9"  onPress={putNewStation} >Ok</Button>
+                <Button
+                  color="#8EA7E9"
+                  onPress={() => {
+                    setStationVisible(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button color="#8EA7E9" onPress={putNewStation}>
+                  Ok
+                </Button>
+              </Dialog.Actions>
+            </Dialog>
+            <Dialog
+              visible={newJourneyDialogVisible}
+              onDismiss={() => {
+                setJourneyVisible(false);
+              }}
+            >
+              <Dialog.Title>New Journey</Dialog.Title>
+              <Dialog.Content>
+                <Text variant="bodyMedium">Enter new journey details:</Text>
+                <MaskedTextInput
+                  placeholder="Departure time"
+                  onChangeText={(text) =>
+                    setNewJourney({ ...newJourney, departure_time: text })
+                  }
+                  mask="9999-99-99T99:99:99"
+                  style={styles.maskedInput}
+                  keyboardType="numeric"
+                />
+                <MaskedTextInput
+                  placeholder="Return time"
+                  onChangeText={(text) =>
+                    setNewJourney({ ...newJourney, return_time: text })
+                  }
+                  mask="9999-99-99T99:99:99"
+                  style={styles.maskedInput}
+                  keyboardType="numeric"
+                />
+                <TextInput
+                  label="Departure Station Id"
+                  keyboardType="numeric"
+                  onChangeText={(text) =>
+                    setNewJourney({ ...newJourney, departure_station_id: text })
+                  }
+                />
+                <TextInput
+                  label="Departure Station"
+                  onChangeText={(text) =>
+                    setNewJourney({ ...newJourney, departure_station: text })
+                  }
+                />
+                <TextInput
+                  label="Return Station Id"
+                  keyboardType="numeric"
+                  onChangeText={(text) =>
+                    setNewJourney({ ...newJourney, return_station_id: text })
+                  }
+                />
+                <TextInput
+                  label="Return Station"
+                  onChangeText={(text) =>
+                    setNewJourney({ ...newJourney, return_station: text })
+                  }
+                />
+                <Text style={styles.text}>
+                  Duration:{" "}
+                  {(Date.parse(newJourney.return_time) -
+                    Date.parse(newJourney.departure_time)) /
+                    1000}{" "}
+                  seconds
+                </Text>
+                <TextInput
+                  label="Distance(m)"
+                  keyboardType="numeric"
+                  onChangeText={(text) =>
+                    setNewJourney({ ...newJourney, distance: text })
+                  }
+                />
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button
+                  color="#8EA7E9"
+                  onPress={() => {
+                    setJourneyVisible(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button color="#8EA7E9" onPress={putNewJourney}>
+                  Ok
+                </Button>
               </Dialog.Actions>
             </Dialog>
           </Portal>
@@ -233,8 +369,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#E5E0FF",
     margin: 10,
   },
-  fixToText: {
-    flexDirection: "column",
-    justifyContent: "space-between",
+  maskedInput: {
+    height: 25,
+    margin: 10,
+  },
+  text: {
+    height: 30,
+    margin: 10,
   },
 });
