339e7616896f8d06355e2a3328cf590226573969
huyggsii <quanghuy.nguyennhu@gmail.com>
2023-01-25 20:47:31 +0200

    moved routes to router

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,12 @@
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const upload = require("./file_upload.js");
-const db = require("./db_queries");
-const { pushCsvToDb } = require("./csv_to_db.js");
+const routes = require("./routes");
 
-
-
-require("dotenv").config();
-
-//express init
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middlewares
+const app = express();
 app.use(bodyParser.json());
 
 app.use(
@@ -22,33 +15,7 @@ app.use(
   })
 );
 
-//back-end main route
-app.get("/", (request, response) => {
-  response.json({
-    info: "Back-end running on Node.js, Express, and PostgresSQL",
-  });
-});
-app.get("/exit",(request, response)=>{
-  response.send("Exiting NodeJS server");
-  console.log("Exiting NodeJS server");
-    process.exit();
-})
-//csv file upload route: stations or journeys
-app.post("/upload/:uploadType", upload.uploadFile.single('file'),upload.uploadCsv);
-
-//csv file parsing route
-app.post("/csv/:importType", pushCsvToDb)
-
-//db queries routes:
-app.post("/db",db.initializeDB);
-app.get("/stations",db.getStations);//get all stations
-app.get("/journeys",db.getJourneys);//get all journeys
-app.get("/station/id=:station_id",db.getStationInfo)//get station infos
-app.get("/journeys/departure=:departure_station_id", db.getJourneysByDepartureStation); //get all journeys starting at a station
-app.get("/journeys/return=:return_station_id", db.getJourneysByReturnStation);//get all journeys ending at a station
-app.post("/journeys/new",db.createJourney);//create new journey based on json object from request body
-app.post("/stations/new",db.createStation);//create new station based on json object from request body
-
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}. Go to http://localhost:3000/`);
