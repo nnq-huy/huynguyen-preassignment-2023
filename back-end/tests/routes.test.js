419d6044fd1167cb7549383aed6d34441b3b2c79
huyggsii <quanghuy.nguyennhu@gmail.com>
2023-01-26 22:28:47 +0200

    added new tests
    new tests for posting new station/ journey f

--- back-end/tests/routes.test.js
+++ back-end/tests/routes.test.js
@@ -3,25 +3,10 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 const request = require("supertest");
 const app = require("../index");
 
-describe("Post to initialize tables /db ", () => {
+describe("POST to initialize tables /db ", () => {
   it("should return success creating new tables:stations, journeys or they exist", async () => {
     const res = await request(app).post("/db");
     expect(res.statusCode).toEqual(201);
-  });
-});
-
-describe("Post to start csv parser /csv/:importType", () => {
-  it("should return error if csv types is not 'stations' or ' journeys'", async () => {
-    const res = await request(app).post("/csv/station");
-    expect(res.statusCode).toEqual(400);
-  });
-  it("should return error if csv types is  'stations' and csv contains existing stations'", async () => {
-    const res = await request(app).post("/csv/stations");
-    expect(res.statusCode).toEqual(400);
-  });
-  it("should return success if csv types is  'journeys' as csv parser already filtered out invalid csv data", async () => {
-    const res = await request(app).post("/csv/journeys");
-    expect(res.statusCode).toEqual(200);
   });
 });
 
@@ -39,18 +24,32 @@ describe("POST /upload/:uploadType to upload file", () => {
   });
 });
 
-describe("POST /journeys/new & /stations/new to create a new station/journey ", () => {
-  it("create new journey based on a valid json object from request body", async () => {
+describe("POST to start csv parser /csv/:importType", () => {
+  it("should return error if csv types is not 'stations' or ' journeys'", async () => {
+    const res = await request(app).post("/csv/station");
+    expect(res.statusCode).toEqual(400);
+  });
+  it("should return error if csv types is  'stations' and csv contains existing stations'", async () => {
+    const res = await request(app).post("/csv/stations");
+    expect(res.statusCode).toEqual(400);
+  });
+  it("should return success if csv types is  'journeys' as csv parser already filtered out invalid csv data", async () => {
+    const res = await request(app).post("/csv/journeys");
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+describe("POST /stations/new to create a new station ", () => {
+  it("create new station based on a valid json object from request body", async () => {
     const randomId = 10000 + Math.round(Math.random() * 10000);
     const data = {
       id: randomId,
       name: "Test station",
       address: "test address",
-      x: 1,
-      y: 1,
+      x: 24.9565,
+      y: 60.161,
     };
     const res = await request(app).post("/stations/new").send(data);
-
     expect(res.statusCode).toEqual(201);
   });
 
@@ -59,19 +58,103 @@ describe("POST /journeys/new & /stations/new to create a new station/journey ", 
       id: 1,
       name: "Test station",
       address: "test address",
-      x: 1,
-      y: 1,
+      x: 24.9565,
+      y: 60.161,
     };
     const res = await request(app).post("/stations/new").send(invalidData);
     expect(res.statusCode).toEqual(405);
   });
-
-  it("return error when no json data is being sent", async () => {
+  it("return error when json data contains invalid latitude/longitude", async () => {
+    const randomId = 10000 + Math.round(Math.random() * 10000);
+    const invalidData = {
+      id: randomId,
+      name: "Test station",
+      address: "test address",
+      x: 10,
+      y: 70,
+    };
+    const res = await request(app).post("/stations/new").send(invalidData);
+    expect(res.statusCode).toEqual(405);
+  });
+  it("return error when json data contains invalid name/address", async () => {
+    const randomId = 10000 + Math.round(Math.random() * 10000);
+    const invalidData = {
+      id: randomId,
+      name: "",
+      address: "test address",
+      x: 24.9565,
+      y: 60.161,
+    };
+    const res = await request(app).post("/stations/new").send(invalidData);
+    expect(res.statusCode).toEqual(405);
+  });
+  it("return error when json data is missing", async () => {
     const res = await request(app).post("/stations/new").send("");
     expect(res.statusCode).toEqual(405);
   });
 });
-
+describe("POST /journeys/new to create a new journey ", () => {
+  it("create new journey based on a valid json object from request body", async () => {
+    const data = {
+      departure_time: "2021-10-10T10:10:10",
+      return_time: "2021-10-10T11:10:10",
+      departure_station_id: 1,
+      departure_station: "Kaivopuisto",
+      return_station_id: 2,
+      return_station: "Laivasillankatu",
+      distance: 100,
+      duration: 100,
+    };
+    const res = await request(app).post("/journeys/new").send(data);
+    expect(res.statusCode).toEqual(201);
+  });
+  it("return error when json data contains invalid departure time", async () => {
+    const invalidData = {
+      departure_time: "2021-25-14T10:10:10",
+      return_time: "2021-10-10T11:10:10",
+      departure_station_id: 1,
+      departure_station: "Kaivopuisto",
+      return_station_id: 2,
+      return_station: "Laivasillankatu",
+      distance: 100,
+      duration: 100,
+    };
+    const res = await request(app).post("/journeys/new").send(invalidData);
+    expect(res.statusCode).toEqual(405);
+  });
+  it("return error when json data contains return time that is invalid/earlier than departure time", async () => {
+    const invalidData = {
+      departure_time: "2021-10-10T11:10:10",
+      return_time: "2021-10-10T10:10:10",
+      departure_station_id: 1,
+      departure_station: "Kaivopuisto",
+      return_station_id: 2,
+      return_station: "Laivasillankatu",
+      distance: 100,
+      duration: 100,
+    };
+    const res = await request(app).post("/journeys/new").send(invalidData);
+    expect(res.statusCode).toEqual(405);
+  });
+  it("return error when json data contains distance/duration that is smaller than 10 meters or seconds", async () => {
+    const invalidData = {
+      departure_time: "2021-10-10T11:10:10",
+      return_time: "2021-10-10T10:10:10",
+      departure_station_id: 1,
+      departure_station: "Kaivopuisto",
+      return_station_id: 2,
+      return_station: "Laivasillankatu",
+      distance: 9,
+      duration: 9,
+    };
+    const res = await request(app).post("/journeys/new").send(invalidData);
+    expect(res.statusCode).toEqual(405);
+  });
+  it("return error when  json data is missing", async () => {
+    const res = await request(app).post("/journeys/new").send("");
+    expect(res.statusCode).toEqual(405);
+  });
+});
 describe("GET /stations", () => {
   it("should return an array of stations", async () => {
     const res = await request(app).get("/stations");
@@ -92,7 +175,6 @@ describe("GET /stations", () => {
   it("should return an error if station is not found", async () => {
     const res = await request(app).get("/station/id=99999");
     expect(res.statusCode).toEqual(404);
-
   });
   it("should throw error if id is not a valid integer", async () => {
     const res = await request(app).get("/station/id=a");
@@ -107,4 +189,3 @@ describe("GET /journeys", () => {
     expect(res.body).toBeTruthy();
   });
 });
-
