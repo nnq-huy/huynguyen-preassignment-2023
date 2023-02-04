3eb09ec7fb17f4e3daf914613f11ca7e6484819c
nnq-huy <quanghuy.nguyennhu@gmail.com>
2023-02-04 22:26:23 +0200

    Revert "flutter initial commit"
    This reverts commit d4ec166a6fe96ab06a6524b903aa91e47f839194.

--- flutter-front-end/android/app/src/main/kotlin/huy/nguyen/citybike/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package huy.nguyen.citybike
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter-front-end/lib/constants/backend.dart
+++
@@ -1,3 +0,0 @@
-// ignore_for_file: constant_identifier_names
-//link to backend
-const String APIURL = 'http://51.195.119.138:3000';
